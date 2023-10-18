ab208c85fddae54ce0c207c10cf936130bd56334
Exiz <yurasankin@yandex.ru>
2023-10-18 16:14:15 +0300

    route with mas

--- src/components/UI/nav/MyLinkList.jsx
+++ src/components/UI/nav/MyLinkList.jsx
@@ -2,11 +2,11 @@ import React, { useState } from 'react'
 import classes from './MyLinkList.module.css'
 import MyLinkItem from './MyLinkItem'
 function MyLinkList() {
-    const [links,setLinks]=useState(
+    let links=
         [
             {id:1,tittle:'Меню',rf:'/'},{id:2,tittle:'Нейросеть',rf:'neuro'},{id:3,tittle:'Поддержка',rf:'help'},{id:4,tittle:'Профиль',rf:'profile'}
         ]
-    )
+    
   return (
     <ul className={classes.nav_list}>
         {links.map(link=> 
